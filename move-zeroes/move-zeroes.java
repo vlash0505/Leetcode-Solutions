f1119e732453426c2a214b40d9f8b32f400ecc05
vlash0505 <87067491+vlash0505@users.noreply.github.com>
2021-11-23 14:22:02 +0200

    Time: 2 ms (46.00%), Space: 40.5 MB (33.51%) - LeetHub

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -1,14 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums.length <= 1) { return; }
-        int count = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != 0) {
-                nums[count++] = nums[i];
-            }
-        }
-        while(count < nums.length) {
-            nums[count++] = 0;
+        for(int i = 0, j = 0; i < nums.length; i++) {
+            if(nums[j] != 0) { j++; }
+            if(i > j && nums[i] != 0) { 
+                nums[j] = nums[i];
+                nums[i] = 0;
+            }                        
         }
     }
 }
